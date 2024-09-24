89fdc397468047729676d89f8aed50b153d2d6fd
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-09-25 01:07:44 +1200

    feat: Setup Axios request to display pokemon data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [pokemonData, setPokemonData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const promises = [];
+        for (let i = 1; i <= 20; i++) {
+          // Fetch first 20 Pokémon
+          promises.push(axios.get(`http://localhost:5000/api/pokemon/${i}`));
+        }
+        const results = await Promise.all(promises);
+        setPokemonData(results.map((res) => res.data));
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(pokemonData);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        {pokemonData.map((pokemon) => (
+          <h1>{pokemon.name}</h1>
+        ))}
+      </div>
     </>
   );
 }
