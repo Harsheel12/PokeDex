eda1e6d0e606164506c899cda4fb4e5548456ecd
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-09-25 00:51:05 +1200

    chore: Setup Express Server

---
+++ backend/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+// Sets our port to the PORT .env value or 4000 by default if .env is not configured
+const PORT = process.env.PORT || 4000;
+
+// Creates the express server
+const app = express();
+
+// Express middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
